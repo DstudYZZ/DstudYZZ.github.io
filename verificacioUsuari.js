c61c3d4f225637c5ba120e944f38bc2e8b050dcb
DstudYZZ <dstudyzz.office@gmail.com>
2023-11-13 13:37:09 +0100

    Informació a apartats

---
+++ verificacioUsuari.js
@@ -0,0 +1,37 @@
+var TextAccesBooleana = localStorage.getItem("acces")
+var acces =  JSON.parse(TextAccesBooleana)
+if (acces){
+}else{
+    
+   window.location.href="404.html"
+}
+
+document.oncontextmenu = function () {
+    
+    return false;
+  };
+  
+  document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+      
+      return false;
+    }
+  
+    if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+      
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode == 85){
+      
+      return false;
+    }
+  };
+  
+  
+  document.onmousedown = function (e) {
+    if (e.button === 2) {
+      
+      return false;
+    }
+  };
+  
